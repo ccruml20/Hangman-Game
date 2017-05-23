e3b8b61d7aa3cf35b643bcf1a5b88821c7446894
ccruml20 <ccrumley1@gmail.com>
2017-05-23 00:33:05 -0600

    updated wins calculations

--- assets/javascript/games.js
+++ assets/javascript/games.js
@@ -1,149 +1,127 @@
-(function(){
+(function() {
 
-// Creating the word Choices Array
+    // Creating the word Choices Array
 
-var wordList = ["Water","Tea","Coke","Beer","Juice","Whisky","Milk","Kombucha","Pepsi",];
+    var wordList = ["Water", "Tea", "Coke", "Beer", "Juice", "Whisky", "Milk", "Kombucha", "Pepsi", ];
 
-// declaring variables
+    // declaring variables
 
-var wins = 0;
-var loses = 0;
-var numberOfGuesses = 10;
-var lettersGuessed = []
-var playerTurns;
-var placeHolders;
-var playerLoses;
-var playerWins;
-var dashMarks;
-var playerPicks;
-var playerGuess;
-var wordChoice;
-var dash;
-var dolose;
-var wordRandom;
-var updateMarks;
+    var wins = 0;
+    var loses = 0;
+    var numberOfGuesses = 10;
+    var lettersGuessed = []
+    var playerTurns;
+    var placeHolders;
+    var playerLoses;
+    var playerWins;
+    var dashMarks;
+    var playerPicks;
+    var playerGuess;
+    var wordChoice;
+    var dash;
+    var dolose;
+    var wordRandom;
+    var updateMarks;
 
+    //setting up the object.
 
-// Random Word Choices
+    var self = {
 
-	var self = {
+        updateWins: function() {
+            playerWins.innerHTML = wins;
 
-	updateWins: function() {
-		// wins;
-	},
-	updateLoses: function() {
-		// loses;
-	},
-	updateGuess: function() {
-		numberOfGuesses;
+        },
+        updateLoses: function() {
+            playerLoses.innerHTML = loses;
 
-	},
-	updateMarks: function() {
-		dash = "";
-		console.log(dash)
-		for (i = 0; i < wordChoice.length; i++) {
-			dash+="-";
-			dashMarks.innerHTML = dash;
-			console.log(dashMarks);
-		}
-	},
-	wordRandom: function(){
-		var wordRandom = Math.floor(Math.random()*wordList.length);
-		wordChoice = wordList[wordRandom].toLowerCase();
-		console.log(wordChoice);
-	},
-	checkGuess: function(letter) {
-		placeHolders = dashMarks.innerHTML;
-		wordLength = wordChoice.length;
-		wrongGuess = true;
-		console.log(placeHolders);
-		placeHolders = placeHolders.split("");
-		console.log(placeHolders.split);
-		console.log(placeHolders);
-		for (i=0; i< wordLength; i++) {
-			if (wordChoice.charAt(i)=== letter) {
-				console.log("im the letter",letter);
-				placeHolders[i] = letter;
-				console.log("complete", letter);
-				wins++;
-				console.log(wins);
-				wrongGuess = false;				
-			}
-		}
-			if (wrongGuess) {
-				console.log(playerGuess);
-				  var newDiv = document.createElement("div");
-				  console.log(newDiv);
-  				  newDiv.innerHTML = letter;
-  				  playerGuess.appendChild(newDiv);
-  				  numberOfGuesses-=1;
-  				  if (numberOfGuesses == 0) {
+        },
+        updateGuess: function() {
+            numberOfGuesses = 10;
+        },
+        updateMarks: function() {
+            dash = "";
+            console.log(dash)
+            for (i = 0; i < wordChoice.length; i++) {
+                dash += "-";
+                dashMarks.innerHTML = dash;
+                console.log(dashMarks);
+            }
+        },
+        wordRandom: function() {
+            wordRandom = Math.floor(Math.random() * wordList.length);
+            wordChoice = wordList[wordRandom].toLowerCase();
+            console.log(wordChoice);
+        },
+        checkGuess: function(letter) {
+            placeHolders = dashMarks.innerHTML;
+            wordLength = wordChoice.length;
+            wrongGuess = true;
+            console.log(placeHolders);
+            placeHolders = placeHolders.split("");
+            console.log(placeHolders.split);
+            console.log(placeHolders);
+            for (i = 0; i < wordLength; i++) {
+                if (wordChoice.charAt(i) === letter) {
+                    console.log("im the letter", letter);
+                    placeHolders[i] = letter;
+                    console.log("complete", letter);
+                    console.log(wins);
+                    wrongGuess = false;
+                }
+            }
+            if (wrongGuess) {
+                console.log(playerGuess);
+                var newDiv = document.createElement("div");
+                console.log(newDiv);
+                newDiv.innerHTML = letter;
+                playerGuess.appendChild(newDiv);
+                numberOfGuesses--;
+                if (numberOfGuesses === 0) {
+                    loses++;
+                    self.updateGuess();
+                }
+                playerLoses.innerHTML = loses;
+                playerPicks.innerHTML = numberOfGuesses;
+                playerWins.innerHTML = wins;
+            }
+            dashMarks.innerHTML = placeHolders.join("");
+            if (placeHolders.join("") === wordChoice) {
+                wins++;
+                self.updateWins();
+                self.updateGuess();
+                self.wordRandom();
+                self.updateMarks();
+            }
+        }
+    }
 
-  				  	loses++; 				
-  				  }
+    document.addEventListener("DOMContentLoaded", function() {
 
-  				  playerLoses.innerHTML = loses;
-  				  playerPicks.innerHTML = numberOfGuesses;
-  				  playerWins.innerHTML = wins;
-
-			}
-		dashMarks.innerHTML = placeHolders.join("");
-
-
-
-
-	}
-
-
-}
-
-
-document.addEventListener("DOMContentLoaded", function(){
-
-	self.wordRandom()
+        self.wordRandom()
 
 
-	playerGuess = document.getElementById("userGuess");
-	self.updateGuess()
+        playerGuess = document.getElementById("userGuess");
+        self.updateGuess()
 
-	playerPicks = document.getElementById("picksLeft");
-	playerPicks.innerHTML = numberOfGuesses;
+        playerPicks = document.getElementById("picksLeft");
+        playerPicks.innerHTML = numberOfGuesses;
 
-	dashMarks = document.getElementById("placeHolder");
-	self.updateMarks()
+        dashMarks = document.getElementById("placeHolder");
+        self.updateMarks()
 
-	playerWins = document.getElementById("wins");
-	self.updateWins()
-	
+        playerWins = document.getElementById("wins");
+        self.updateWins()
 
-	playerLoses = document.getElementById("loses");
-	self.updateLoses() 
 
-})
-document.addEventListener("keydown", function(event){
-	self.checkGuess(event.key)
-})
+        playerLoses = document.getElementById("loses");
+        self.updateLoses()
+
+    })
+    document.addEventListener("keydown", function(event) {
+        self.checkGuess(event.key)
+    })
 
 
 
 
 }());
-
-
-
-
-
-	
-
-
-
-
-
-
-
-
-
-
-
-
-
